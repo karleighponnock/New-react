d51fb2870b657b89d23dfa1dde2c915a2bdde170
karleighponnock <karleighponnock@gmail.com>
2020-09-03 17:50:56 -0400

    set low health point when hp is 30 send to hospital and break at that point so it doesnt loop endlessly

--- attack.py
+++ attack.py
@@ -9,10 +9,11 @@ while playerhp > 0:
     dmg = random.randrange(enemyatkl, enemyatkh)
     playerhp = playerhp - dmg
 
-    if playerhp <= 0:
-        playerhp = 0
+    if playerhp <= 30:
+        playerhp = 30
         
-    print("Enemy strikes for", dmg, "points of damage. Current HP is", playerhp) 
+    print("Enemy strikes for", dmg, "points of damage. Current HP is", playerhp)
     
-if playerhp == 0:
-    print("You have died. You cannot respawn")
+    if playerhp == 30:
+        print("You have low heslth. You have teleported to the nearest hospital")
+        break;
