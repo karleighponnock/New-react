fe821eca06ee4fa5edb723d6964db166d468e1a8
karleighponnock <karleighponnock@gmail.com>
2020-09-03 19:10:19 -0400

    get static hp from each enemy

--- attack.py
+++ attack.py
@@ -2,20 +2,26 @@ import random
 
 
 class Enemy:
-    
+ #each instance of an object are their own object   
     def __init__(self, atkl, atkh):
         self.atkl = atkl
         self.atkh = atkh
         
 
     def getAtk(self):
-        print(self.atkl)
+        print("atk is", self.atkl)
+        
+    
+    def getHp(self):
+        print("Hp is", self.hp )
         
 enemy1 = Enemy(40, 49)
 enemy1.getAtk()
+enemy1.getHp()
 
 enemy2 = Enemy(75, 90)
 enemy2.getAtk()
+enemy2.getHp()
 
 # #set variables
 # playerhp = 260
