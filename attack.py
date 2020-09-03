21b679e01db2f399c513a13d49aafa710d8a8360
karleighponnock <karleighponnock@gmail.com>
2020-09-03 17:46:41 -0400

    Add message when player dies

--- attack.py
+++ attack.py
@@ -9,4 +9,10 @@ while playerhp > 0:
     dmg = random.randrange(enemyatkl, enemyatkh)
     playerhp = playerhp - dmg
 
+    if playerhp <= 0:
+        playerhp = 0
+        
     print("Enemy strikes for", dmg, "points of damage. Current HP is", playerhp) 
+    
+if playerhp == 0:
+    print("You have died. You cannot respawn")
