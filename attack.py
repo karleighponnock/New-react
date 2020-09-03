f5821137cfcd74a959c18bfe919688f723569472
karleighponnock <karleighponnock@gmail.com>
2020-09-03 18:59:10 -0400

    use object for enemies and create 2

--- attack.py
+++ attack.py
@@ -1,26 +1,40 @@
 import random 
 
-#set variables
-playerhp = 260
-enemyatkl = 60
-enemyatkh = 80
 
-#as long as the player is alive run
-while playerhp > 0:
-    #generate hit point and subtract it from player hp
-    dmg = random.randrange(enemyatkl, enemyatkh)
-    playerhp = playerhp - dmg
+class Enemy:
+    atkl = 60
+    atkh = 80
 
-#set low health point
-    if playerhp <= 30:
-        playerhp = 30
+    def getAtk(self):
+        print(self.atkl)
         
-    print("Enemy strikes for", dmg, "points of damage. Current HP is", playerhp)
-#if player hits low health print message
+enemy1 = Enemy()
+enemy1.getAtk()
 
-    if playerhp > 30:
-        continue
+enemy2 = Enemy()
+enemy2.getAtk()
+
+# #set variables
+# playerhp = 260
+# enemyatkl = 60
+# enemyatkh = 80
+
+# #as long as the player is alive run
+# while playerhp > 0:
+#     #generate hit point and subtract it from player hp
+#     dmg = random.randrange(enemyatkl, enemyatkh)
+#     playerhp = playerhp - dmg
+
+# #set low health point
+#     if playerhp <= 30:
+#         playerhp = 30
         
-    print("You have low health. You have teleported to the nearest hospital")
-    #break out of while loop
-    break;
+#     print("Enemy strikes for", dmg, "points of damage. Current HP is", playerhp)
+# #if player hits low health print message
+
+#     if playerhp > 30:
+#         continue
+        
+#     print("You have low health. You have teleported to the nearest hospital")
+#     #break out of while loop
+#     break;
