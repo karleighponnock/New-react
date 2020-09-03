22a43411a098676b80100366c91efcc86f01751d
karleighponnock <karleighponnock@gmail.com>
2020-09-03 17:53:02 -0400

    add comments

--- attack.py
+++ attack.py
@@ -1,19 +1,23 @@
 import random 
 
-
+#set variables
 playerhp = 260
 enemyatkl = 60
 enemyatkh = 80
 
+#as long as the player is alive run
 while playerhp > 0:
+    #generate hit point and subtract it from player hp
     dmg = random.randrange(enemyatkl, enemyatkh)
     playerhp = playerhp - dmg
 
+#set low health point
     if playerhp <= 30:
         playerhp = 30
         
     print("Enemy strikes for", dmg, "points of damage. Current HP is", playerhp)
-    
+#if player hits low health print message
     if playerhp == 30:
-        print("You have low heslth. You have teleported to the nearest hospital")
+        print("You have low health. You have teleported to the nearest hospital")
+        #break out of while loop
         break;
