be1a0fed3c0e255f890e8159b73ac070297fe52a
karleighponnock <karleighponnock@gmail.com>
2020-09-03 15:25:10 -0400

    function and list spacing examples

--- main.py
+++ main.py
@@ -3,3 +3,17 @@
 import os
 import sys
 from MyModule import fop, bar, foobar
+
+def my_function(one, two,
+                three, four, 
+                five, six):
+    print("Hello World")
+
+#two bank lines to seperate functions
+def second_function():
+    print("Second function")
+
+
+my_list = [1, 2,
+            3, 4,
+            5, 6]
