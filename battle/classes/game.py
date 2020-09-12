457019cae801f3bd607f5390025d2f53b1dd7240
karleighponnock <karleighponnock@gmail.com>
2020-09-12 10:38:38 -0400

    initialize Person stats

---
+++ battle/classes/game.py
@@ -0,0 +1,25 @@
+class bcolors:
+    HEADER    = '\033[95m'
+    OKBLUE    = '\033[94m'
+    OKGREEN   = '\033[92m'
+    WARNING   = '\033[93m'
+    FAIL      = '\033[91m'
+    BOLD      = '\033[1m'
+    UNDERLINE = '\033[4m'
+    ENDC      = '\033[0m'
+    
+    
+    #initiallize stats from parameter
+class Person:
+    def __init__(self, hp, mp, atk, df, magic):
+        self.maxhp = hp
+        self.hp = hp
+        self.maxmp = mp
+        self.mp = mp
+        self.stkl = atk - 10
+        self.atkh = atk + 10
+        self.df = df
+        self.magic = magic
+        self.actions = ["Attack", "magic"]
+
+        
