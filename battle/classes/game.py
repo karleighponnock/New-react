2840ca1be808eeff0bffe131d8c86c6b2b51fe98
karleighponnock <karleighponnock@gmail.com>
2020-09-12 10:44:55 -0400

    intantiate a new player and create three common magic spells

--- battle/classes/game.py
+++ battle/classes/game.py
@@ -1,3 +1,5 @@
+import random 
+
 class bcolors:
     HEADER    = '\033[95m'
     OKBLUE    = '\033[94m'
@@ -22,4 +24,7 @@ class Person:
         self.magic = magic
         self.actions = ["Attack", "magic"]
 
-        
+    def generate_damage(self):
+        return random.randrange(self.atkl, self.atkh)
+    
+    
